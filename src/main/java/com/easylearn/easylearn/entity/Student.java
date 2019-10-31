f27923759cbeab74e2fbe84c0f63c7771eb9c4e2
Mahmoud_Abdelrahman <mahmoud.abou-auf@outlook.com>
2019-10-31 23:56:39 +0100

    Finished Database Code

--- src/main/java/com/easylearn/easylearn/entity/Student.java
+++ src/main/java/com/easylearn/easylearn/entity/Student.java
@@ -6,28 +6,5 @@ import java.util.Set;
 @Entity
 public class Student {
 
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY) // Auto Increament
-    private Long id;
 
-    @Column(nullable = false)
-    private String firstName;
-
-    @Column(nullable = false)
-    private String lastName;
-
-    @Column(unique = true, nullable = false)
-    private String email;
-
-    @Column(nullable = false)
-    private String password;
-
-    @ManyToMany(mappedBy = "students", cascade = CascadeType.PERSIST, fetch = FetchType.EAGER)
-    private Set<Appointment> appointments;
-
-    @ManyToMany(mappedBy = "students", cascade = CascadeType.PERSIST, fetch = FetchType.EAGER)
-    private Set<Course> courses;
-
-    @ManyToOne(fetch = FetchType.LAZY)
-    private Parent parent;
 }
