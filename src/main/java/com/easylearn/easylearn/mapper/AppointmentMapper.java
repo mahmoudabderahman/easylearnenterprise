5d37ceaa0c87bab28e3fa1e4c187a0f1d7a80a34
Mahmoud_Abdelrahman <mahmoud.abou-auf@outlook.com>
2019-11-07 23:22:42 +0100

    Adding some content to mappers

--- src/main/java/com/easylearn/easylearn/mapper/AppointmentMapper.java
+++ src/main/java/com/easylearn/easylearn/mapper/AppointmentMapper.java
@@ -6,6 +6,7 @@ import com.easylearn.easylearn.model.AppointmentRespDTO;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.HashSet;
 import java.util.Set;
 
 @Component
@@ -14,22 +15,39 @@ public class AppointmentMapper implements ObjectMapper<Appointment, AppointmentR
 
     @Override
     public Appointment mapToEntity(AppointmentReqDTO request) {
-        return null;
+        return Appointment.builder()
+                .startDate(request.getStartDate())
+                .endDate(request.getEndDate())
+                .roomNumber(request.getRoomNumber())
+                .build();
     }
 
     @Override
-    public Appointment mapToEntity(Appointment entity, AppointmentReqDTO request) {
-        return null;
+    public Appointment mapToEntity(Appointment appointment, AppointmentReqDTO request) {
+        appointment.setStartDate(request.getStartDate());
+        appointment.setEndDate(request.getEndDate());
+        appointment.setRoomNumber(request.getRoomNumber());
+        return appointment;
     }
 
     @Override
-    public AppointmentRespDTO mapToDTO(Appointment entity) {
-        return null;
+    public AppointmentRespDTO mapToDTO(Appointment appointment) {
+        return AppointmentRespDTO.builder()
+                .id(appointment.getId())
+                .startDate(appointment.getStartDate())
+                .endDate(appointment.getEndDate())
+                .roomNumber(appointment.getRoomNumber())
+                .build();
     }
 
     @Override
-    public Set<AppointmentRespDTO> mapToDTOs(Set<Appointment> entities) {
-        return null;
+    public Set<AppointmentRespDTO> mapToDTOs(Set<Appointment> appointments) {
+        if (appointments == null || appointments.isEmpty())
+            return null;
+
+        Set<AppointmentRespDTO> appointmentsItr = new HashSet<>();
+        appointments.forEach(appointment -> appointmentsItr.add(mapToDTO(appointment)));
+        return appointmentsItr;
     }
 
 }
---
+++ src/main/java/com/easylearn/easylearn/mapper/StudentMapper.java
@@ -0,0 +1,58 @@
+package com.easylearn.easylearn.mapper;
+
+import com.easylearn.easylearn.entity.Student;
+import com.easylearn.easylearn.model.StudentReqDTO;
+import com.easylearn.easylearn.model.StudentRespDTO;
+import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.HashSet;
+import java.util.Set;
+// id, firstName, lastName, email, password
+
+@Component
+@Transactional
+public class StudentMapper implements ObjectMapper<Student, StudentReqDTO, StudentRespDTO> {
+    @Override
+    public Student mapToEntity(StudentReqDTO request) {
+        return Student.builder()
+                .firstName(request.getFirstName())
+                .lastName(request.getLastName())
+                .email(request.getEmail())
+                .password(request.getPassword())
+                .build();
+    }
+
+    @Override
+    public Student mapToEntity(Student student, StudentReqDTO request) {
+        student.setFirstName(request.getFirstName());
+        student.setLastName(request.getLastName());
+        student.setEmail(request.getEmail());
+        student.setPassword(request.getPassword());
+        return student;
+    }
+
+    @Override
+    public StudentRespDTO mapToDTO(Student student) {
+        return StudentRespDTO.builder()
+                .id(student.getId())
+                .firstName(student.getFirstName())
+                .lastName(student.getLastName())
+                .email(student.getEmail())
+                .password(student.getPassword())
+                .build();
+    }
+
+    @Override
+    public Set<StudentRespDTO> mapToDTOs(Set<Student> students) {
+        if (students == null || students.isEmpty())
+            return null;
+
+        Set<StudentRespDTO> studentsItr = new HashSet<>();
+        students.forEach(student -> studentsItr.add(mapToDTO(student)));
+        return studentsItr;
+
+    }
+}
+
+
