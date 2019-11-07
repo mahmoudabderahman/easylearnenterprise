a3c21e419eb565d5454dbc973acbc19016515db6
Mahmoud_Abdelrahman <mahmoud.abou-auf@outlook.com>
2019-11-07 01:37:53 +0100

    Adding Mappers and fixing some bugs in the controllers

--- src/main/java/com/easylearn/easylearn/mapper/AppointmentMapper.java
+++ src/main/java/com/easylearn/easylearn/mapper/AppointmentMapper.java
@@ -31,49 +31,5 @@ public class AppointmentMapper implements ObjectMapper<Appointment, AppointmentR
     public Set<AppointmentRespDTO> mapToDTOs(Set<Appointment> entities) {
         return null;
     }
-    /*
-    @Override
-    public Exercise mapToEntity(ExerciseReqDTO request) {
-        return Exercise.builder()
-                .name(request.getName())
-                .description(request.getDescription())
-                .type(request.getType())
-                .numberOfRounds(request.getNumberOfRounds())
-                .duration(request.getDuration())
-                .build();
-    }
 
-    @Override
-    public Exercise mapToEntity(Exercise exercise, ExerciseReqDTO request) {
-        exercise.setName(request.getName());
-        exercise.setDescription(request.getDescription());
-        exercise.setType(request.getType());
-        exercise.setNumberOfRounds(request.getNumberOfRounds());
-        exercise.setDuration(request.getDuration());
-        return exercise;
-    }
-
-    @Override
-    public ExerciseRespDTO mapToDTO(Exercise entity) {
-        return ExerciseRespDTO.builder()
-                .id(entity.getId())
-                .name(entity.getName())
-                .description(entity.getDescription())
-                .type(entity.getType())
-                .numberOfRounds(entity.getNumberOfRounds())
-                .duration(entity.getDuration())
-                .workoutId(entity.getWorkout().getId())
-                .build();
-    }
-
-    @Override
-    public Set<ExerciseRespDTO> mapToDTOs(Set<Exercise> entities) {
-        if (entities == null || entities.isEmpty())
-            return null;
-
-        Set<ExerciseRespDTO> exercises = new HashSet<>();
-        entities.forEach(exercise -> exercises.add(mapToDTO(exercise)));
-        return exercises;
-    }
-     */
 }
