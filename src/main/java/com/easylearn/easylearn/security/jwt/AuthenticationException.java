73641f91f11a74a03907bb8a46abb86ed1a12c16
fixcjmf <fixcjmf@linux.fritz.box>
2020-01-20 21:45:07 +0100

    Deleted security package files

--- src/main/java/com/easylearn/easylearn/security/jwt/AuthenticationException.java
+++
@@ -1,8 +0,0 @@
-package com.easylearn.easylearn.security.jwt;
-
-public class AuthenticationException extends RuntimeException {
-    public AuthenticationException(String message, Throwable cause) {
-        super(message, cause);
-
-    }
-}
--- src/main/java/com/easylearn/easylearn/security/jwt/JwtTokenAuthorizationOncePerRequestFilter.java
+++
@@ -1,8 +0,0 @@
-package com.easylearn.easylearn.security.jwt;
-
-import org.springframework.stereotype.Component;
-import org.springframework.web.filter.OncePerRequestFilter;
-
-@Component
-public class JwtTokenAuthorizationOncePerRequestFilter extends OncePerRequestFilter {
-}
