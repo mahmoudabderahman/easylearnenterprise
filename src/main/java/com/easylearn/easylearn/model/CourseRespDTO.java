022f621fe1149bbe037e64b30fea9d75910bee6b
Mahmoud Abdelrahman <30775657+MahmoudAbderahman@users.noreply.github.com>
2021-02-07 17:07:23 +0100

    Update CourseRespDTO.java

--- src/main/java/com/easylearn/easylearn/model/CourseRespDTO.java
+++ src/main/java/com/easylearn/easylearn/model/CourseRespDTO.java
@@ -1,5 +1,5 @@
 /**
- * @Author: Mahmoud Abdelrahman, Steve Titinang
+ * @Author: Mahmoud Abdelrahman
  * Course Response Data Transfer Object is where the specifications required for responses declared.
  */
 package com.easylearn.easylearn.model;
