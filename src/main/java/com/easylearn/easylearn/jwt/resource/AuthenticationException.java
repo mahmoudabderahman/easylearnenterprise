b9851ccd34e46fdc4408f91db8efbe8ec66e10ff
Mahmoud Abdelrahman <30775657+MahmoudAbderahman@users.noreply.github.com>
2021-02-07 17:03:54 +0100

    Update AuthenticationException.java

--- src/main/java/com/easylearn/easylearn/jwt/resource/AuthenticationException.java
+++ src/main/java/com/easylearn/easylearn/jwt/resource/AuthenticationException.java
@@ -1,5 +1,5 @@
 /**
- * @Author: Mahmoud Abdelrahman, Steve Titinang
+ * @Author: Mahmoud Abdelrahman
  * Authentication Exception class is where all AuthenticationException specifications are declared.
  */
 package com.easylearn.easylearn.jwt.resource;
