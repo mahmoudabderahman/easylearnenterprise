e5cf3806e9bf90a5cc952f5e3ed1dbdee4b770e0
Mahmoud Abdelrahman <30775657+MahmoudAbderahman@users.noreply.github.com>
2021-02-07 17:20:22 +0100

    Update CourseService.java

--- src/main/java/com/easylearn/easylearn/service/CourseService.java
+++ src/main/java/com/easylearn/easylearn/service/CourseService.java
@@ -1,5 +1,5 @@
 /**
- * @Author: Mahmoud Abdelrahman, Steve Titinang
+ * @Author: Mahmoud Abdelrahman
  * Course Service class is where the code responsible for implementing the course controller methods
  * implemented.
  */
