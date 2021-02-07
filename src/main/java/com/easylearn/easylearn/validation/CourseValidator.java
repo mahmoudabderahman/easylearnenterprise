2596e3e07388ec128faa863dd41293a037db6456
Mahmoud Abdelrahman <30775657+MahmoudAbderahman@users.noreply.github.com>
2021-02-07 17:21:40 +0100

    Update CourseValidator.java

--- src/main/java/com/easylearn/easylearn/validation/CourseValidator.java
+++ src/main/java/com/easylearn/easylearn/validation/CourseValidator.java
@@ -1,5 +1,5 @@
 /**
- * @Author: Mahmoud Abdelrahman, Steve Titinang
+ * @Author: Mahmoud Abdelrahman
  * Course Validator class is where the code responsible for implementing the course validator methods
  * implemented.
  * Extends the BaseValidator class.
