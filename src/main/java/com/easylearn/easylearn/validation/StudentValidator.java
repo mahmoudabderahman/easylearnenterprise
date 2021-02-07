6c4893b6eea28b666fe936e6bfb6c58e6de31da6
Mahmoud Abdelrahman <30775657+MahmoudAbderahman@users.noreply.github.com>
2021-02-07 17:22:02 +0100

    Update StudentValidator.java

--- src/main/java/com/easylearn/easylearn/validation/StudentValidator.java
+++ src/main/java/com/easylearn/easylearn/validation/StudentValidator.java
@@ -1,5 +1,5 @@
 /**
- * @Author: Mahmoud Abdelrahman, Steve Titinang
+ * @Author: Mahmoud Abdelrahman
  * Student Validator class is where the code responsible for implementing the student validator methods
  * implemented.
  * Extends the BaseValidator class.
