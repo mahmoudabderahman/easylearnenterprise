a25d7c102e702cf112b970ed35a5244489699791
Mahmoud Abdelrahman <30775657+MahmoudAbderahman@users.noreply.github.com>
2021-02-07 17:04:07 +0100

    Update JwtAuthenticationRestController.java

--- src/main/java/com/easylearn/easylearn/jwt/resource/JwtAuthenticationRestController.java
+++ src/main/java/com/easylearn/easylearn/jwt/resource/JwtAuthenticationRestController.java
@@ -1,5 +1,5 @@
 /**
- * @Author: Mahmoud Abdelrahman, Steve Titinang
+ * @Author: Mahmoud Abdelrahman
  * JwtAuthentication Controller is a class, where all create, refresh
  * and handle exceptions of tokens implemented.
  */
