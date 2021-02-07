ce38ffbe2e642def192b82154ab194dce799d4b1
Mahmoud Abdelrahman <30775657+MahmoudAbderahman@users.noreply.github.com>
2021-02-07 17:21:50 +0100

    Update ParentValidator.java

--- src/main/java/com/easylearn/easylearn/validation/ParentValidator.java
+++ src/main/java/com/easylearn/easylearn/validation/ParentValidator.java
@@ -1,5 +1,5 @@
 /**
- * @Author: Mahmoud Abdelrahman, Steve Titinang
+ * @Author: Mahmoud Abdelrahman
  * Parent Validator class is where the code responsible for implementing the parent validator methods
  * implemented.
  * Extends the BaseValidator class.
